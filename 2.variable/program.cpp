3d410ece25ff05f2d99271f541ab09a117c21317
purva bhokare <purvab08@gmail.com>
2022-02-01 23:23:14 +0530

    variable understanding

---
+++ 2.variable/program.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main (void)
+{
+    // addition of two number
+    // variable declaration
+    float a,b;
+    // variable initialization
+    a = 10.5; 
+    b = 2;
+    int mul=2;
+    mul=4; 
+    mul=5;
+    float c = a + b;
+    printf ("addition of a and b is %f",c);
+    printf ("\n %d x 1 = %d",mul, mul*1);
+    printf ("\n %d x 2 = %d",mul, mul*2);
+    printf ("\n %d x 3 = %d",mul, mul*3);
+    printf ("\n %d x 4 = %d",mul, mul*4);
+    printf ("\n %d x 5 = %d",mul, mul*5);
+    printf ("\n %d x 6 = %d",mul, mul*6);
+    printf ("\n %d x 7 = %d",mul, mul*7);
+    printf ("\n %d x 8 = %d",mul, mul*8);
+    printf ("\n %d x 9 = %d",mul, mul*9);
+    printf ("\n %d x 10 = %d",mul, mul*10);
+    
+
+    
+
+    }
