5d7904541bfa2a35c05ec282249636d89cf94416
purva bhokare <purvab08@gmail.com>
2022-02-02 11:41:00 +0530

    practice of variables

---
+++ new program/program.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main (void)
+{
+    // addition of two numbers
+    // variable declaration
+    int a,b;
+    // variable initialization
+    a = 10;
+    b = 12;
+    int c = a + b;
+    printf ("addition of a + b is %d", c);
+}
